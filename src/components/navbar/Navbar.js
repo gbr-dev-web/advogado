c24a10fd28a2a1efe75d812ae4a610488c1271b1
gabriel <gabrielsil20177@gmail.com>
2025-07-01 15:31:48 -0300

    navbar00

--- src/components/navbar/Navbar.js
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-    return (
-        <nav>
-            <h1>eu quero gozar</h1>
-        </nav>
-    );
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Navbar from "@/components/navbar/Navbar";
+import Header from "@/components/header/Header";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <main></main>
+      <Header />
+      <main className="h-[4000px] bg-amber-50"></main>
       <footer></footer>
     </div>
   );
